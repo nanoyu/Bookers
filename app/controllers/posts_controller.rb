f60bf510971040e6c3b7e34d0e875d14c5237b19
永野裕也 <naganoyuuya@yuuya.local>
2018-11-15 17:36:43 +0900

    [finish]4-8

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  def new
+  	@post = Post.new
+  	@posts = Post.all
+  end
+
+  def create
+  	  post = Post.new(post_params)
+   if post.save
+      flash[:notice] = "Book was successfully created"
+  	  redirect_to post_path(post.id)
+   end
+  end
+
+  def index
+  	  @posts = Post.all
+  end
+
+  def show
+  	  @post = Post.find(params[:id])
+      @posts = Post.all
+  end
+
+  def edit
+  	  @post = Post.find(params[:id])
+      @posts = Post.all
+  end
+
+  def update
+  	  post = Post.find(params[:id])
+   if post.update(post_params)
+      flash[:notice] = "Book was successfully updated"
+  	  redirect_to post_path(post.id)
+   end
+  end
+
+  def destroy
+  	  post = Post.find(params[:id])
+  	  post.destroy
+  	  redirect_to posts_new_path
+  end
+
+  private
+
+  def post_params
+  	  params.require(:post).permit(:title, :body)
+  end
+end
